2866495beb755bdb5ab72d562cb4a4ffd2e6e7a4
Marco <Marco@CLANK>
2023-04-30 00:53:35 -0400

    Add project files.

---
+++ Templates/Templates.cpp
@@ -0,0 +1,23 @@
+// Marcus Needham
+// CIS 1202 501
+// April 30 2023
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T> T half(T);
+int half(int);
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+template <typename T> T half(T num) {
+
+}
+
+int half(int num) {
+
+}
